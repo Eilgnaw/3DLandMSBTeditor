6b5c931a0f1130b5522e524d22ecaee722fe25d9
exelix11 <ex.elix@libero.it>
2015-12-18 17:10:57 +0100

    Revert "Changed the encoding on the buffered string from UTF8 to ASCII becaus…"

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MsbtEditor
+namespace _3DlandMSBTeditor
 {
-	static class Program
-	{
-		/// <summary>
-		/// Punto di ingresso principale dell'applicazione.
-		/// </summary>
-		[STAThread]
-		static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new frmMain(args));
-		}
-	}
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(args));
+        }
+    }
 }
